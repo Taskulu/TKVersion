8ca231c136ebcf537f50ed966bae1fd08a412b3c
Arash Ranjbaran <arash@taskulu.com>
2017-05-14 13:05:31 +0430

    Initial Commit

---
+++ TKVersion/TKVersion.h
@@ -0,0 +1,19 @@
+//
+//  TKVersion.h
+//  TKVersion
+//
+//  Created by Arash on 5/14/17.
+//  Copyright © 2017 Taskulu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TKVersion.
+FOUNDATION_EXPORT double TKVersionVersionNumber;
+
+//! Project version string for TKVersion.
+FOUNDATION_EXPORT const unsigned char TKVersionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TKVersion/PublicHeader.h>
+
+
